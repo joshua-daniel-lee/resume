e599eb3d4841f6c367adda7b8825ebf0656eabbb
Joshua Lee <jdl93@nau.edu>
2018-06-10 09:41:28 -1000

    started to add components

--- resume/src/containers/Header.js
+++ resume/src/containers/Header.js
@@ -1,17 +1,23 @@
 import React, { Component } from "react";
-import Button from "@material-ui/core/Button";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import { withStyles } from "@material-ui/core/styles";
 
 class Header extends Component {
   constructor() {
     super();
   }
-
   render() {
     return (
       <div className="header">
-        <Button variant="raised" color="primary">
-          Hello World
-        </Button>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="title" color="secondary">
+              Joshua Daniel Lee
+            </Typography>
+          </Toolbar>
+        </AppBar>
       </div>
     );
   }
--- resume/src/index.js
+++ resume/src/index.js
@@ -1,8 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Landing from "./Landing";
+import registerServiceWorker from "./registerServiceWorker";
+import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import purple from "@material-ui/core/colors/purple";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const theme = createMuiTheme({
+  palette: {
+    primary: { main: purple[500] },
+    secondary: { main: "#11cb5f" }
+  }
+});
+
+ReactDOM.render(
+  <MuiThemeProvider theme={theme}>
+    <div>
+      <Landing />
+    </div>
+  </MuiThemeProvider>,
+
+  document.getElementById("root")
+);
 registerServiceWorker();
