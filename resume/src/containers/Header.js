5bdef35dcd3dd45783cf7049481833e1ac3a7e42
Joshua Lee <jdl93@nau.edu>
2018-06-10 09:52:02 -1000

    add an app bar

--- resume/src/containers/Header.js
+++ resume/src/containers/Header.js
@@ -2,7 +2,6 @@ import React, { Component } from "react";
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
-import { withStyles } from "@material-ui/core/styles";
 
 class Header extends Component {
   constructor() {
--- resume/src/index.js
+++ resume/src/index.js
@@ -4,12 +4,11 @@ import "./index.css";
 import Landing from "./Landing";
 import registerServiceWorker from "./registerServiceWorker";
 import { MuiThemeProvider, createMuiTheme } from "@material-ui/core/styles";
-import purple from "@material-ui/core/colors/purple";
 
 const theme = createMuiTheme({
   palette: {
-    primary: { main: purple[500] },
-    secondary: { main: "#11cb5f" }
+    primary: { main: "#193549" },
+    secondary: { main: "#FFFFFF" }
   }
 });
 
