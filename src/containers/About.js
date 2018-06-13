6012a77f9295c73fca69a3a24f0d09bb0992ba0c
Joshua Lee <jdl93@nau.edu>
2018-06-13 05:06:51 -1000

    cleanding the directory

---
+++ src/containers/About.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+import Paper from "@material-ui/core/Paper";
+
+class About extends Component {
+  // constructor() {
+  //   super();
+  // }
+
+  render() {
+    return (
+      <div className="about">
+        <Paper elevation={4}>hello world</Paper>
+      </div>
+    );
+  }
+}
+
+export default About;
