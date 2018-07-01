cfbf87c8d4586b1a57655c4204b916856965112b
Joshua Lee <jdl93@nau.edu>
2018-06-30 20:59:00 -1000

    adding the about section

--- src/containers/About/About.js
+++ src/containers/About/About.js
@@ -1,35 +1,17 @@
 import React from "react";
 import "./about.css";
 import Paper from "@material-ui/core/Paper";
-import Typography from "@material-ui/core/Typography";
-import List from "@material-ui/core/List";
-import ListItem from "@material-ui/core/ListItem";
-import ListItemText from "@material-ui/core/ListItemText";
-import Avatar from "@material-ui/core/Avatar";
-import ImageIcon from "@material-ui/icons/Image";
-import WorkIcon from "@material-ui/icons/FitnessCenter";
-import BeachAccessIcon from "@material-ui/icons/BeachAccess";
-import Divider from "@material-ui/core/Divider";
+import Logo from "../../assets/Logo.png";
 
 function About(props) {
   return (
     <div className="about">
-      <div className="aboutContentOne">
-        <div className="aboutHeadline">
-          <Typography
-            color="primary"
-            variant="display2"
-            align="center"
-            gutterBottom
-          >
-            THE GIST
-          </Typography>
-        </div>
-      </div>
+      <div className="aboutContentOne" />
       <div className="aboutContentTwo" />
       <div className="aboutContentTwo" />
       <Paper className="aboutContainerOne" elevation={8} />
       <Paper className="aboutContainerTwo" elevation={8} />
+      <img className="imageLogo" src={Logo} alt="Logo" />
     </div>
   );
 }
