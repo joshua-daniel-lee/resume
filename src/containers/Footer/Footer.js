d6c0381a7234a1a6f9d232bec455c2f47a86d19c
Joshua Lee <jdl93@nau.edu>
2018-09-22 16:07:34 -1000

    tag your it

--- src/containers/Footer/Footer.js
+++ src/containers/Footer/Footer.js
@@ -1,11 +1,12 @@
 import React from "react";
+import Avatar from "@material-ui/core/Avatar";
 import "./footer.css";
 import Logo from "../../assets/Logo.png";
 
 function Footer(props) {
   return (
     <div className="footer">
-      <img className="footerLogo" src={Logo} alt="Logo" />
+      <Avatar className="footerLogo" alt="Logo" src={Logo} />
     </div>
   );
 }
